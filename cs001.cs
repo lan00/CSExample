84df781ecaea940ffa8c4120502ffe7890547b43
lan00 <37057028+lan00@users.noreply.github.com>
2018-08-08 20:17:33 -0700

    add CloseChildProcess function

--- cs001.cs
+++ cs001.cs
@@ -8,3 +8,51 @@
             System.Threading.Thread.Sleep(10000);
             i.Quit();
         }
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Diagnostics;*/
+
+namespace CloseChildProcess
+{
+// CloseChildProcess  11:16 AM 8/9/2018
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pid;
+            string strPId;
+            Console.Write("please input a number : ");
+            strPId=Console.ReadLine();
+            if (int.TryParse(strPId, out pid))
+                KillProcessAndChildren(pid);
+            else
+                Console.WriteLine("input number error");
+
+            Console.ReadKey();
+        }
+
+        public static void KillProcessAndChildren(int pid)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                Console.WriteLine(pid); proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                /* process already exited */
+            }
+        }
+    }
+}
